786423d49d7f7d345a37f9a1d749e5ac0186ce05
Matthew Lane <themplane@gmail.com>
2020-07-14 19:56:46 -0700

    I did it!

--- src/main.js
+++ src/main.js
@@ -1,211 +1,232 @@
-var articleSavedGrid = document.querySelector('.saved-posters-grid');
+var buttonBackFromNewPoster = document.querySelector('.show-main');
 var buttonBackFromSaved = document.querySelector('.back-to-main');
-var buttonBackFromUserPoster = document.querySelector('.show-main');
-var buttonMakeYourOwnPoster = document.querySelector('.show-form');
+var buttonCreateUserPoster = document.querySelector('.make-poster');
+var buttonNewRandomPoster = document.querySelector('.show-random');
 var buttonSavePoster = document.querySelector('.save-poster');
-var buttonShowMyPoster = document.querySelector('.make-poster');
-var buttonShowRandom = document.querySelector('.show-random');
-var buttonShowSaved = document.querySelector('.show-saved');
+var buttonShowSavedGrid = document.querySelector('.show-saved');
+var buttonShowUserForm = document.querySelector('.show-form');
 var inputQuote = document.querySelector('#poster-quote');
 var inputTitle = document.querySelector('#poster-title');
 var inputURL = document.querySelector('#poster-image-url');
+var mainPosterDisplay = document.querySelector('.main-poster');
+var newPosterForm = document.querySelector('.poster-form');
 var posterImage = document.querySelector('.poster-img');
-var posterQuote = document.querySelector('.poster-quote');
 var posterTitle = document.querySelector('.poster-title');
-var sectionMainPoster = document.querySelector('.main-poster');
-var sectionPosterForm = document.querySelector('.poster-form');
-var sectionSavedPosters = document.querySelector('.saved-posters');
+var posterQuote = document.querySelector('.poster-quote');
+var savedGrid = document.querySelector('.saved-posters-grid')
+var savedSection = document.querySelector('.saved-posters');
+var buttonFlash;
 
 var images = [
-  './assets/bees.jpg',
-  './assets/bridge.jpg',
-  './assets/butterfly.jpg',
-    './assets/cliff.jpg',
-  './assets/elephant.jpg',
-  './assets/flock.jpg',
-  './assets/fox.jpg',
-  './assets/frog.jpg',
-  './assets/horse.jpg',
-  './assets/lion.jpg',
-  './assets/mountain.jpg',
-  './assets/pier.jpg',
-  './assets/puffins.jpg',
-  './assets/pug.jpg',
-  './assets/runner.jpg',
-  './assets/squirrel.jpg',
-  './assets/tiger.jpg',
-  './assets/turtle.jpg'
-];
-
-var quotes = [
-  'Don’t downgrade your dream just to fit your reality, upgrade your conviction to match your destiny.',
-  'You are braver than you believe, stronger than you seem and smarter than you think.',
-  'You are confined only by the walls you build yourself.',
-  'The one who has confidence gains the confidence of others.',
-  'Act as if what you do makes a difference. It does.',
-  'Success is not final, failure is not fatal: it is the courage to continue that counts.',
-  'Never bend your head. Always hold it high. Look the world straight in the eye.',
-  'What you get by achieving your goals is not as important as what you become by achieving your goals.',
-  'Believe you can and you\'re halfway there.',
-  'When you have a dream, you\'ve got to grab it and never let go.',
-  'I can\'t change the direction of the wind, but I can adjust my sails to always reach my destination.',
-  'No matter what you\'re going through, there\'s a light at the end of the tunnel.',
-  'It is our attitude at the beginning of a difficult task which, more than anything else, will affect its successful outcome.',
-  'Life is like riding a bicycle. To keep your balance, you must keep moving.',
-  'Just don\'t give up trying to do what you really want to do. Where there is love and inspiration, I don\'t think you can go wrong.',
-  'Limit your \'always\' and your \'nevers.\'',
-  'You are never too old to set another goal or to dream a new dream.',
-  'Try to be a rainbow in someone else\'s cloud.',
-  'You do not find the happy life. You make it.',
-  'Inspiration comes from within yourself. One has to be positive. When you\'re positive, good things happen.',
-  'Sometimes you will never know the value of a moment, until it becomes a memory.',
-  'The most wasted of days is one without laughter.',
-  'You must do the things you think you cannot do.',
-  'It isn\'t where you came from. It\'s where you\'re going that counts.',
-  'It is never too late to be what you might have been.',
-  'Happiness often sneaks in through a door you didn\'t know you left open.',
-  'We must be willing to let go of the life we planned so as to have the life that is waiting for us.',
-  'Never limit yourself because of others’ limited imagination; never limit others because of your own limited imagination.',
-  'Be the change that you wish to see in the world.',
-  'Let us make our future now, and let us make our dreams tomorrow\'s reality.',
-  'You don\'t always need a plan. Sometimes you just need to breathe, trust, let go, and see what happens.',
-  'If I cannot do great things, I can do small things in a great way.',
-  'Don\'t wait. The time will never be just right.',
-  'With the right kind of coaching and determination you can accomplish anything.',
-  'If you have good thoughts they will shine out of your face like sunbeams and you will always look lovely.',
-  'No matter what people tell you, words and ideas can change the world.',
-  'Each person must live their life as a model for others.',
-  'A champion is defined not by their wins but by how they can recover when they fall.'
+  "./assets/bees.jpg",
+  "./assets/bridge.jpg",
+  "./assets/butterfly.jpg",
+  "./assets/cliff.jpg",
+  "./assets/elephant.jpg",
+  "./assets/flock.jpg",
+  "./assets/fox.jpg",
+  "./assets/frog.jpg",
+  "./assets/horse.jpg",
+  "./assets/lion.jpg",
+  "./assets/mountain.jpg",
+  "./assets/pier.jpg",
+  "./assets/puffins.jpg",
+  "./assets/pug.jpg",
+  "./assets/runner.jpg",
+  "./assets/squirrel.jpg",
+  "./assets/tiger.jpg",
+  "./assets/turtle.jpg"
 ];
 
 var titles = [
-  'determination',
-  'success',
-  'inspiration',
-  'perspiration',
-  'grit',
-  'empathy',
-  'feelings',
-  'hope',
-  'believe',
-  'try',
-  'conviction',
-  'accomplishment',
-  'achievement',
-  'ambition',
-  'clarity',
-  'challenge',
-  'commitment',
-  'confidence',
-  'action',
-  'courage',
-  'focus',
-  'breathe',
-  'gratitude',
-  'imagination',
-  'kindness',
-  'mindfulness',
-  'knowledge',
-  'opportunity',
-  'passion',
-  'patience',
-  'practice',
-  'smile',
-  'trust',
-  'understanding',
-  'wisdom'
+  "determination",
+  "success",
+  "inspiration",
+  "perspiration",
+  "grit",
+  "empathy",
+  "feelings",
+  "hope",
+  "believe",
+  "try",
+  "conviction",
+  "accomplishment",
+  "achievement",
+  "ambition",
+  "clarity",
+  "challenge",
+  "commitment",
+  "confidence",
+  "action",
+  "courage",
+  "focus",
+  "breathe",
+  "gratitude",
+  "imagination",
+  "kindness",
+  "mindfulness",
+  "knowledge",
+  "opportunity",
+  "passion",
+  "patience",
+  "practice",
+  "smile",
+  "trust",
+  "understanding",
+  "wisdom"
+];
+var quotes = [
+  "Don’t downgrade your dream just to fit your reality, upgrade your conviction to match your destiny.",
+  "You are braver than you believe, stronger than you seem and smarter than you think.",
+  "You are confined only by the walls you build yourself.",
+  "The one who has confidence gains the confidence of others.",
+  "Act as if what you do makes a difference. It does.",
+  "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+  "Never bend your head. Always hold it high. Look the world straight in the eye.",
+  "What you get by achieving your goals is not as important as what you become by achieving your goals.",
+  "Believe you can and you're halfway there.",
+  "When you have a dream, you've got to grab it and never let go.",
+  "I can't change the direction of the wind, but I can adjust my sails to always reach my destination.",
+  "No matter what you're going through, there's a light at the end of the tunnel.",
+  "It is our attitude at the beginning of a difficult task which, more than anything else, will affect its successful outcome.",
+  "Life is like riding a bicycle. To keep your balance, you must keep moving.",
+  "Just don't give up trying to do what you really want to do. Where there is love and inspiration, I don't think you can go wrong.",
+  'Limit your "always" and your "nevers."',
+  "You are never too old to set another goal or to dream a new dream.",
+  "Try to be a rainbow in someone else's cloud.",
+  "You do not find the happy life. You make it.",
+  "Inspiration comes from within yourself. One has to be positive. When you're positive, good things happen.",
+  "Sometimes you will never know the value of a moment, until it becomes a memory.",
+  "The most wasted of days is one without laughter.",
+  "You must do the things you think you cannot do.",
+  "It isn't where you came from. It's where you're going that counts.",
+  "It is never too late to be what you might have been.",
+  "Happiness often sneaks in through a door you didn't know you left open.",
+  "We must be willing to let go of the life we planned so as to have the life that is waiting for us.",
+  "Never limit yourself because of others’ limited imagination; never limit others because of your own limited imagination.",
+  "Be the change that you wish to see in the world.",
+  "Let us make our future now, and let us make our dreams tomorrow's reality.",
+  "You don't always need a plan. Sometimes you just need to breathe, trust, let go, and see what happens.",
+  "If I cannot do great things, I can do small things in a great way.",
+  "Don't wait. The time will never be just right.",
+  "With the right kind of coaching and determination you can accomplish anything.",
+  "If you have good thoughts they will shine out of your face like sunbeams and you will always look lovely.",
+  "No matter what people tell you, words and ideas can change the world.",
+  "Each person must live their life as a model for others.",
+  "A champion is defined not by their wins but by how they can recover when they fall."
 ];
 
 var savedPosters = [];
-var currentPoster;
 var lastClicked;
 
-articleSavedGrid.addEventListener('click', clickTwiceThenDelete);
-buttonBackFromSaved.addEventListener('click', goBackFromDisplaySaved);
-buttonBackFromUserPoster.addEventListener('click', toggleFormDisplay);
-buttonMakeYourOwnPoster.addEventListener('click', toggleFormDisplay);
+buttonBackFromNewPoster.addEventListener('click', function toggleFormDisplay2 (){
+  toggleDisplay(newPosterForm);
+});
+
+buttonBackFromSaved.addEventListener('click', function toggleSavedDisplay (){
+  toggleDisplay(savedSection);
+});
+
+buttonCreateUserPoster.addEventListener('click', makeUserPoster);
+buttonNewRandomPoster.addEventListener('click', makeRandomPoster);
 buttonSavePoster.addEventListener('click', storeShownPoster);
-buttonShowMyPoster.addEventListener('click', makeUserPoster);
-buttonShowRandom.addEventListener('click', makeRandomPoster);
-buttonShowSaved.addEventListener('click', toggleSaveButton);
+buttonShowSavedGrid.addEventListener('click', toggleSaveButton);
+buttonShowUserForm.addEventListener('click', function toggleFormDisplay (){
+  toggleDisplay(newPosterForm);
+});
+savedSection.addEventListener('click', clickTwiceThenDelete);
 
-;function clickTwiceThenDelete(event) {
-  var indexNumber = event.target.id.split('-')[1];
-  if (lastClicked === indexNumber) {
-    savedPosters.splice(indexNumber, 1);
-    var deleteMe = document.querySelector(`#mini-${indexNumber}`);
-    articleSavedGrid.removeChild(deleteMe);
-    lastClicked = false;
+
+function clickTwiceThenDelete(event) {
+  var posterIDNumber = event.target.id.split('-')[2]
+  if (lastClicked === posterIDNumber) {
+    var deleteMe = document.querySelector(`#mini-poster-${posterIDNumber}`);
+    savedGrid.removeChild(deleteMe);
+    savedPosters.splice(posterIDNumber, 1);
+    lastClicked = null;
   } else {
-    lastClicked = indexNumber;
+    lastClicked = posterIDNumber;
   }
 };
 
-;function displaySavedGrid() {
-  articleSavedGrid.innerHTML = '';
-  for (var i = 0; i < savedPosters.length; i++) {
-    var newHTML =
-      `<article class='mini-poster' id='mini-${i}'>
-        <img class='poster-img' id='miniIMG-${i}' src='${savedPosters[i].imageURL}' alt='This is a motivational picture about ${savedPosters[i].title}.'>
-        <h2 class='poster-title' id='miniTTL-${i}'>${savedPosters[i].title}</h2>
-        <h4 class='poster-quote' id='miniQTE-${i}'>${savedPosters[i].quote}</h4>
-      </article>`;
-    articleSavedGrid.innerHTML += newHTML;
-  }
-};
-
-;function getRandomIndex(array) {
+function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 };
 
-;function goBackFromDisplaySaved() {
-  sectionMainPoster.classList.toggle('hidden');
-  sectionSavedPosters.classList.toggle('hidden');
-};
-
-;function makePoster(imgURL, title, quote) {
-  var newPoster = new Poster(imgURL, title, quote);
-  currentPoster = newPoster;
+function makePoster(imgURL, title, quote) {
+  currentPoster = new Poster(imgURL, title, quote);
   posterImage.src = currentPoster.imageURL;
   posterImage.alt = `This is a motivational picture about ${currentPoster.title}.`;
   posterTitle.innerText = currentPoster.title;
   posterQuote.innerText = currentPoster.quote;
 };
 
-;function makeRandomPoster() {
+function makeRandomPoster() {
   var newURL = images[getRandomIndex(images)];
   var newTitle = titles[getRandomIndex(titles)];
   var newQuote = quotes[getRandomIndex(quotes)];
   makePoster(newURL, newTitle, newQuote);
 };
 
-;function makeUserPoster(event) {
+function makeUserPoster(event) {
+  event.preventDefault();
   var newURL = inputURL.value;
   var newTitle = inputTitle.value;
   var newQuote = inputQuote.value;
-  event.preventDefault();
   images.push(newURL);
   titles.push(newTitle);
   quotes.push(newQuote);
   makePoster(newURL, newTitle, newQuote);
-  toggleFormDisplay();
+  toggleDisplay(newPosterForm);
 };
 
-;function storeShownPoster() {
-  if (currentPoster !== savedPosters[savedPosters.length - 1]) {
-    savedPosters.push(currentPoster);
+function toggleDisplay(node){
+  mainPosterDisplay.classList.toggle('hidden');
+  node.classList.toggle('hidden')
+  buttonFlash.classList.remove('success')
+  buttonFlash.classList.remove('fail')
+}
+
+function storeShownPoster() {
+  if (savedPosters[savedPosters.length - 1] !== currentPoster) {
+    savedPosters.push(currentPoster)
+    buttonFlash.classList.toggle('dont-fade')
+    buttonFlash.classList.toggle('not-shown')
+    buttonFlash.innerText = 'SAVED'
+    buttonFlash.classList.remove('fail')
+    buttonFlash.classList.add('success')
+    window.setTimeout(function(){
+      buttonFlash.classList.toggle('dont-fade')
+      buttonFlash.classList.toggle('not-shown')
+    })  // turn on fade
+  } else {
+    buttonFlash.classList.toggle('dont-fade')
+    buttonFlash.classList.toggle('not-shown')
+    buttonFlash.innerText = 'NO'
+    buttonFlash.classList.remove('success')
+    buttonFlash.classList.add('fail')
+    window.setTimeout(function(){
+      buttonFlash.classList.toggle('dont-fade')
+      buttonFlash.classList.toggle('not-shown')
+    })
   }
 };
 
-;function toggleFormDisplay() {
-  sectionMainPoster.classList.toggle('hidden');
-  sectionPosterForm.classList.toggle('hidden');
-};
-
-;function toggleSaveButton() {
-  sectionMainPoster.classList.toggle('hidden');
-  sectionSavedPosters.classList.toggle('hidden');
-  displaySavedGrid();
+function toggleSaveButton() {
+  toggleDisplay(savedSection);
+  savedGrid.innerHTML = '';
+  for (var i = 0; i < savedPosters.length; i++) {
+    var newHTML =
+      `<article class="mini-poster" id="mini-poster-${i}">
+      <img class="poster-img" id="mini-img-${i}" src="${savedPosters[i].imageURL}" alt="This is a motivational picture about ${savedPosters[i].title}.">
+      <h2 class="poster-title" id="mini-title-${i}">${savedPosters[i].title}</h2>
+      <h4 class="poster-quote" id="mini-quote-${i}">${savedPosters[i].quote}</h4>
+    </article>`;
+    savedGrid.innerHTML += newHTML;
+  };
 };
 
 makeRandomPoster();
+
+buttonSavePoster.insertAdjacentHTML(`beforebegin`, `<button class='fade not-shown'>SAVED</button>`)
+buttonFlash = document.querySelector('.fade')
